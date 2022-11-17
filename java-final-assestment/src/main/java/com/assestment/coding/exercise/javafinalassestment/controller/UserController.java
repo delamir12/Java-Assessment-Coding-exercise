c5aac82139d8e382e277171736f17cda31b1b1c7
Estebanst99 <estebanbena20@gmail.com>
2022-11-17 11:32:44 +0100

    API finished

---
+++ java-final-assestment/src/main/java/com/assestment/coding/exercise/javafinalassestment/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.assestment.coding.exercise.javafinalassestment.controller;
+
+
+import com.assestment.coding.exercise.javafinalassestment.entity.User;
+import com.assestment.coding.exercise.javafinalassestment.service.impl.UserServiceImpl;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserController {
+
+    @Autowired
+    private UserServiceImpl userService;
+
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> listAllUsers(){
+        List<User> users = userService.getAllUsers();
+
+        if (users.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(users);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@PathVariable("id") int id) {
+
+        User user = userService.getUserById(id);
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(user);
+    }
+
+    @PostMapping()
+    public ResponseEntity<User> createUser(@RequestBody User user){
+        User newUser = userService.createUser(user);
+        return ResponseEntity.ok(newUser);
+    }
+
+
+
+
+
+}
---
+++ java-final-assestment/src/main/java/com/assestment/coding/exercise/javafinalassestment/entity/User.java
@@ -0,0 +1,62 @@
+package com.assestment.coding.exercise.javafinalassestment.entity;
+
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private String email;
+
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
