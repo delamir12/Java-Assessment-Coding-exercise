ef3fa04a18583ea310f94be20c0d9b90fd46968c
Estebanst99 <estebanbena20@gmail.com>
2022-11-17 09:46:25 +0100

    App

---
+++ java-final-assestment/src/main/java/com/assestment/coding/exercise/javafinalassestment/JavaFinalAssestmentApplication.java
@@ -0,0 +1,13 @@
+package com.assestment.coding.exercise.javafinalassestment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFinalAssestmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaFinalAssestmentApplication.class, args);
+	}
+
+}
---
+++ java-final-assestment/src/test/java/com/assestment/coding/exercise/javafinalassestment/JavaFinalAssestmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assestment.coding.exercise.javafinalassestment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaFinalAssestmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
